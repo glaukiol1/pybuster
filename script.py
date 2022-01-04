8e5a6c20108dd1213394d003a8d8062a54e2391f
glaukiol1 <glaukiol1@gmail.com>
2022-01-04 16:12:41 +0100

    added pattern in URL

--- script.py
+++ script.py
@@ -4,7 +4,8 @@ def _run(words,_url,_s,pipe,id):
         if pipe['run'] == False:
             break
         try:
-            response = requests.get(_url+word)
+
+            response = requests.get(_url.replace("PYBUSTER", word)+word)
             if _s.find(str(response.status_code))!=-1:
                 pipe['found'] = pipe['found'] + 1
                 pipe['dirs'].append(word)
