42dd6e94f9071cd0d6ea69230237b1650f6e3e14
glaukiol1 <glaukiol1@gmail.com>
2022-01-04 15:39:28 +0100

    Changes to outputting and script

--- pybuster.py
+++ pybuster.py
@@ -6,7 +6,7 @@ with open(w, 'r') as file:
     wordlist = file.read().splitlines()
 url = sys.argv[2]
 
-threads = 10
+threads = int(input("#> Threads: "))
 
 s = sys.argv[3]
 
@@ -23,19 +23,44 @@ by Glaukio L (@glaukiol1) | Inspired by gobuster
 [+] Url/Domain   : {url}
 [+] Wordlist     : {w}
 [+] Status codes : {s}
+[+] Threads:     : {threads}
+===============================================================
+#> data             Output pybuster data
+#> exit             Exit out all the threads & exit main instance
 ===============================================================
 {out_time} Starting pybuster
-===============================================================
+
 """)
 from threading import Thread
 
 import script
 import splittingt
-pipe = []
+pipe = {"found": 0, "dirs": [], "total_done": 0, "errors": 0, "total": wordlist.__len__(), "run": True}
+ts = []
 def main():
-    for x in range(10):
-        Thread(target=script._run,args=(splittingt.main(wordlist,threads)[x],url,s,pipe)).start()
+    for x in range(threads):
+        Thread(target=script._run,args=(splittingt.main(wordlist,threads)[x],url,s,pipe,x)).start()
 if __name__  == '__main__':
     main()
-    print(pipe)
-
+    starttime = time.time()
+    print(f'[+] {time.localtime().tm_hour}:{time.localtime().tm_min}: pybuster threads started')
+    while True:
+        m = input("#> ")
+        if m == 'data':
+            print(f"Dir's Tested: {pipe['total_done']}/{pipe['total']} | Dir's Found: {pipe['found']} | Hard Errors: {pipe['errors']} | Time: {int(time.time()-starttime)} seconds")
+        if m == 'exit':
+            pipe["run"] = False
+            break
+    print(f"""
+    \n
+===============================================================
+Directories checked: {pipe["total_done"]}/{pipe["total"]} (total)
+Directories found: {pipe["found"]}
+Hard errors: {pipe["errors"]}
+Time: {int(time.time()-starttime)} seconds
+Directory URLs that were found:
+===============================================================
+    {pipe["dirs"]}
+    """)
+    print("Quitting all instances, you may do CTRL+C to speed this up")
+    sys.exit()
--- script.py
+++ script.py
@@ -1,7 +1,14 @@
-def _run(words,_url,_s,pipe):
-    pipe.append('HELLO!')
+def _run(words,_url,_s,pipe,id):
     import requests
     for word in words:
-        response = requests.get(_url+word)
-        if _s.find(str(response.status_code))!=-1:
-            print("[*] Found /"+word+" | Status: "+str( response.status_code ))
+        if pipe['run'] == False:
+            break
+        try:
+            response = requests.get(_url+word)
+            if _s.find(str(response.status_code))!=-1:
+                pipe['found'] = pipe['found'] + 1
+                pipe['dirs'].append(word)
+        except:
+            pipe['errors'] = pipe['errors'] + 1
+        pipe['total_done'] = pipe['total_done'] + 1
+   # finished {id}
