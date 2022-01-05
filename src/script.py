f272600a051674182ae1b9efb0dd3fe2b32020b4
glaukiol1 <glaukiol1@gmail.com>
2022-01-05 10:50:24 +0100

    performance changes

--- src/script.py
+++ src/script.py
@@ -27,11 +27,13 @@ def _run(words,_url,_s,pipe,id,mode):
             if pipe['run'] == False:
                 break
             try:
-                response = requests.get(outstr.replace("PYBUSTER",word))
+                s = outstr.replace("PYBUSTER",word) # this will be faster than to call the function 3 times
+                s_len = s.__len__()
+                response = requests.get(s)
                 if _s.find(str(response.status_code))!=-1:
                     pipe['found'] = pipe['found'] + 1
                     pipe['dirs'].append(word)
-                    print(f'= {outstr.replace("PYBUSTER",word)} {" "*(33-(outstr.replace("PYBUSTER",word).__len__()+str(id).__len__()))}(Status: {response.status_code}) (Found by: #{id})  =')
+                    print(f'= {s} {" "*(33-(s_len+str(id).__len__()))}(Status: {response.status_code}) (Found by: #{id})  =')
             except:
                 pipe['errors'] = pipe['errors'] + 1
             pipe['total_done'] = pipe['total_done'] + 1
